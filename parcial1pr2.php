be6fe71f14f3d3164755a5954b12f1118599f7ca
Edgard Cabrera <edgardmauriciop@gmail.com>
2016-03-06 11:10:33 -0600

    Primer parcial de programacion 2

---
+++ parcial1pr2.php
@@ -0,0 +1,12 @@
+<?php
+//$num=readline("Ingrese un valo : ");
+$num=readline("Ingrese un numero: ");
+if($num%2){
+	echo "Este numero es par: ".$num."\n";
+}else{
+	echo "Este numero es impar: ".$num."\n";
+}
+?>
+// Edgard Mauricio Cabrera Peraza
+// Carnet CP01132863
+// https://github.com/edgardmauriciop/parcial1pr2-uls.git
